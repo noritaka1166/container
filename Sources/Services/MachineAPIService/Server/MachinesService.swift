44374a237bea69342a2fffe46dbcf8681f419eca
Kathryn Baldauf <k_baldauf@apple.com>
2026-08-19 09:44:25 -0700

    Do not set default maskedPaths and readonlyPaths for container machines (#2137)
    Signed-off-by: Kathryn Baldauf <k_baldauf@apple.com>

--- Sources/Services/MachineAPIService/Server/MachinesService.swift
+++ Sources/Services/MachineAPIService/Server/MachinesService.swift
@@ -697,6 +697,8 @@ extension MachineConfiguration {
         config.capAdd = ["ALL"]
         config.ssh = true
         config.virtualization = virtualization
+        config.readonlyPaths = []
+        config.maskedPaths = []
 
         config.rosetta = platform.architecture == "amd64" && Arch.hostArchitecture() == .arm64
 
