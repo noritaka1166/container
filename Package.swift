04324afe634839ed9c3990c77ec25d1ae5c9e47e
Raj <rajaryan_singh@apple.com>
2026-08-20 10:31:01 -0700

    Add identifier validation tests for the disk-usage routes (#2136)

--- Package.swift
+++ Package.swift
@@ -253,9 +253,14 @@ let package = Package(
             dependencies: [
                 .product(name: "Containerization", package: "containerization"),
                 "ContainerAPIService",
+                "ContainerAPIClient",
+                "ContainerPersistence",
+                "ContainerPlugin",
                 "ContainerResource",
                 "ContainerRuntimeLinuxClient",
                 "ContainerRuntimeClient",
+                "ContainerTestSupport",
+                "ContainerXPC",
             ]
         ),
         .target(
