33ebc8ae67a06d148b3492dccfeb42ab00317470
Eric Ernst <eric_ernst@apple.com>
2026-09-11 12:08:27 -0700

    forwarder: close backend channel immediately if frontend closes (#2260)
    Signed-off-by: Eric Ernst <eric_ernst@apple.com>

--- Sources/SocketForwarder/ConnectHandler.swift
+++ Sources/SocketForwarder/ConnectHandler.swift
@@ -62,7 +62,7 @@ extension ConnectHandler {
                 case .success(let channel):
                     guard context.channel.isActive else {
                         self.log?.trace("backend - frontend channel closed, closing backend connection")
-                        context.channel.close(promise: nil)
+                        channel.close(promise: nil)
                         return
                     }
                     self.log?.trace("backend - connected")
